9e3d9bb9299b1558946cb3f9300d7c06f838c520
alepaez-dev <al3_hp@hotmail.com>
2021-01-19 14:30:41 -0700

    ordenamiento ya

--- js/prueba.js
+++ js/prueba.js
@@ -2,12 +2,6 @@ console.log("estamos en prueba.js");
 
 //basealljquery
 const baseAllJquery = (method, funct, data, path, url) => {
-  //   console.log("entra a la funcion de baseAllJquery");
-  //   console.log("metodo: ", method);
-  //   console.log("data:", data);
-  //   console.log("path", path);
-  //   console.log("funct: ", funct);
-  //   console.log("url:", url);
   if (method === "GET" || method === "DELETE") {
     console.log("entro al if de GET");
     $.ajax({
@@ -51,69 +45,122 @@ const baseAllJquery = (method, funct, data, path, url) => {
 
 //showusers
 const showUsers = (response, path) => {
-  let usersList = "";
-  console.log("path", path);
-  console.log("usersList:", usersList);
+  let listrasusuarios = [];
+  let listanueva = "";
+
+  // convertir object a pasar a arreglo
   for (user in response) {
-    let usuario = response[user];
-    console.log("user:", usuario);
-    usersList += `<a href="#">
-                                <article class="article-main d-flex m-1">
-                                    <div class="iconos-article mr-2">
-                                        <img class="icono-article" src="${usuario.urlImg}" alt="">
-                                    </div>
-                                    <div class="article-info mt-1 mr-1">
-                                        <div class="nombre-fecha">
-                                            <h5 class="nombre-article">
-                                                ${usuario.author}
-                                            </h5>
-                                            <p class="fecha-article">${usuario.dateCreated}</p>
+    let usuar = response[user];
+    let data = Object.assign({}, { _id: user, ...usuar });
+    listrasusuarios.push(data);
+  }
+
+  //ordenar ese mismo arreglo
+  listrasusuarios.sort((a, b) => {
+    return new Date(b.dateCreated) - new Date(a.dateCreated);
+  });
+
+  //agregar
+  listrasusuarios.forEach(element => {
+    console.log(element);
+    console.log(element.dateCreated);
+    listanueva += `<a href="#">
+                                    <article class="article-main d-flex m-1">
+                                        <div class="iconos-article mr-2">
+                                            <img class="icono-article" src="${element.urlPhoto}" alt="">
                                         </div>
-                                        <div class="titulo-tags">
-                                            <h3>${usuario.title}</h3>
-                                            <a href="#">#${usuario.tag}</a>
-                                            <a href="#">#software</a>
-                                            <a href="#">#improvement</a>
-                                            <a href="#">#kidding</a>
-                                            <a href="#">#sad</a>
-                                        </div>
-                                        <div
-                                            class="iconos-botones-article d-flex flex-row justify-content-between align-items-baseline mt-3">
-                                            <div class="iconos-botones-iconos">
-                                                <i class="far fa-heart"></i>
-                                                8
-                                                <i class="far fa-comment ml-2"></i>
-                                                3
+                                        <div class="article-info mt-1 mr-1">
+                                            <div class="nombre-fecha">
+                                                <h5 class="nombre-article">
+                                                    ${element.author}
+                                                </h5>
+                                                <p class="fecha-article">${element.dateCreated}</p>
+                                            </div>
+                                            <div class="titulo-tags">
+                                                <h3>${element.title}</h3>
+                                                <a href="#">#${element.tag}</a>
+                                                <a href="#">#software</a>
+                                                <a href="#">#improvement</a>
+                                                <a href="#">#kidding</a>
+                                                <a href="#">#sad</a>
                                             </div>
                                             <div
-                                                class="botones-der d-flex flex-row justify-content-between align-items-baseline">
-                                                <p>${usuario.minsToRead} read</p>
-                                                <button class="btn-save">Save</button>
-                                            </div>
+                                                class="iconos-botones-article d-flex flex-row justify-content-between align-items-baseline mt-3">
+                                                <div class="iconos-botones-iconos">
+                                                    <i class="far fa-heart"></i>
+                                                    8
+                                                    <i class="far fa-comment ml-2"></i>
+                                                    3
+                                                </div>
+                                                <div
+                                                    class="botones-der d-flex flex-row justify-content-between align-items-baseline">
+                                                    <p>${element.minsToRead} read</p>
+                                                    <button class="btn-save">Save</button>
+                                                </div>
 
+                                            </div>
                                         </div>
-                                    </div>
-                                </article>
-                            </a>`;
-  }
-  console.log("path", path);
-  console.log("usersList:", usersList);
-  console.log(window.location.pathname);
-  console.log("blkabla");
-  console.log("path", path);
-  console.log("usersList:", usersList);
-  $(path).append(usersList);
+                                    </article>
+                                </a>`;
+  });
+
+  //   for (user in response) {
+  //     let usuario = response[user];
+  //     console.log(response[user].dateCreated);
+  //     // console.log("response: ", response);
+  //     // console.log("user:", usuario);
+  //     usersList += `<a href="#">
+  //                                     <article class="article-main d-flex m-1">
+  //                                         <div class="iconos-article mr-2">
+  //                                             <img class="icono-article" src="${usuario.urlPhoto}" alt="">
+  //                                         </div>
+  //                                         <div class="article-info mt-1 mr-1">
+  //                                             <div class="nombre-fecha">
+  //                                                 <h5 class="nombre-article">
+  //                                                     ${usuario.author}
+  //                                                 </h5>
+  //                                                 <p class="fecha-article">${usuario.dateCreated}</p>
+  //                                             </div>
+  //                                             <div class="titulo-tags">
+  //                                                 <h3>${usuario.title}</h3>
+  //                                                 <a href="#">#${usuario.tag}</a>
+  //                                                 <a href="#">#software</a>
+  //                                                 <a href="#">#improvement</a>
+  //                                                 <a href="#">#kidding</a>
+  //                                                 <a href="#">#sad</a>
+  //                                             </div>
+  //                                             <div
+  //                                                 class="iconos-botones-article d-flex flex-row justify-content-between align-items-baseline mt-3">
+  //                                                 <div class="iconos-botones-iconos">
+  //                                                     <i class="far fa-heart"></i>
+  //                                                     8
+  //                                                     <i class="far fa-comment ml-2"></i>
+  //                                                     3
+  //                                                 </div>
+  //                                                 <div
+  //                                                     class="botones-der d-flex flex-row justify-content-between align-items-baseline">
+  //                                                     <p>${usuario.minsToRead} read</p>
+  //                                                     <button class="btn-save">Save</button>
+  //                                                 </div>
+
+  //                                             </div>
+  //                                         </div>
+  //                                     </article>
+  //                                 </a>`;
+  //   }
+  $(path).append(listanueva);
   //   deleteUserBtns();
 };
+
 urlFirebase =
   "https://retofrontend-81a79-default-rtdb.firebaseio.com/posts/.json";
-urlImg = "./assets/article-image-1.jpeg";
-urlPhoto = "./assets/imagen-main-2.png";
+urlImg = "https://loremflickr.com/320/240/person";
+urlPhoto = "https://loremflickr.com/320/240/person";
 
 let post = {
   author: "Rahul Mishra",
   content: "content",
-  dateCreated: new Date(Date.now()).toISOString().slice(0, 10),
+  dateCreated: new Date("2018-03-02").toISOString().slice(0, 10),
   minsToRead: "1",
   tag: "tag",
   title: "How to Ruin Your Career in 4 Easy Steps!!",
@@ -129,5 +176,5 @@ $(document).ready(() => {
   console.log("entranod a jqury");
   console.log(post);
   //   baseAllJquery("POST", "", post, "", urlFirebase);
-  baseAllJquery("GET", showUsers, "", ".aaapp", urlFirebase);
+  baseAllJquery("GET", showUsers, "", ".list__usuarios", urlFirebase);
 });
