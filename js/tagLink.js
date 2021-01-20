46af3077601d3bfe83aefd522e098cea1e3ca61f
msalda <msalda53@gmail.com>
2021-01-20 12:46:37 -0600

    tags form

--- js/tagLink.js
+++ js/tagLink.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     console.log("documento listo")
-    
     $("#tagsFormat").click(function () {
         console.log("boton activado")
         $("#tagsFormat").css("color","red")
