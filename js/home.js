cf4f10d23bff49d3e7a9fa6dfa68325e8426dcb3
alepaez-dev <al3_hp@hotmail.com>
2021-01-20 14:50:25 -0700

    listo_2

--- js/home.js
+++ js/home.js
@@ -8,69 +8,6 @@ urlPhoto = "https://loremflickr.com/320/240/person";
 
 //POST GET
 
-let post_1 = {
-  author: "Rahul Mishra",
-  content: `You may think a career is an old fashioned concept out of the 80s which could be safely ignored. I sure used to… I thought a career was for banking professionals in suits, not for me! I was young and cool and wanted to work for a living, of course, but I couldn't care less about climbing up the career ladder. Well, I changed my mind.`,
-  dateCreated: new Date("2020-03-02").toISOString().slice(0, 10),
-  minsToRead: "1",
-  tag: "github, markdown, webdev, readme",
-  title: "How to Ruin Your Career in 4 Easy Steps!!",
-  urlAuthor: "/assets/article-image-1.jpeg",
-  urlPhoto: "/assets/imagen-main-2.png",
-};
-
-let post_2 = {
-  author: "Jared White",
-  content:
-    "I think the folks building Tailwind are talented and nice people. But at a pure technical level, I simply don't like",
-  dateCreated: new Date("2018-02-02").toISOString().slice(0, 10),
-  minsToRead: "12",
-  tag: "tag",
-  title: "webdev, css, javascript, html",
-  urlAuthor: "/assets/article-image-2.jpg",
-  urlPhoto: "https://dev.to/social_previews/article/573028.png",
-};
-
-let post_3 = {
-  author: "Ben Halpern",
-  content:
-    "Cumulative layout shift is the term that refers to content that jumps after initial load.  Not only i...",
-  dateCreated: new Date("2016-03-02").toISOString().slice(0, 10),
-  minsToRead: "60",
-  tag: "github, markdown, webdev, readme",
-  title: "Fixing Cumulative Layout Shift",
-  urlAuthor: "/assets/article-image-3.png",
-  urlPhoto:
-    "https://res.cloudinary.com/practicaldev/image/fetch/s--mhUvOpdy--/c_imagga_scale,f_auto,fl_progressive,h_500,q_66,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/v91nxntizymsobq46ug6.gif",
-};
-
-let post_4 = {
-  author: "Braydon Coyer",
-  content: "Creating a Killer GitHub Profile README Part 1",
-  dateCreated: new Date("2017-03-02").toISOString().slice(0, 10),
-  minsToRead: "12",
-  tag: "webdev, css, javascript, html",
-  title: "I wrote a free book on React.js and just made it available on GitHub",
-  urlAuthor: "/assets/article-image-5.jpeg",
-  urlPhoto:
-    "https://res.cloudinary.com/practicaldev/image/fetch/s--wFKC0VlN--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/552620/1529d884-64fa-4a05-91a5-336c12488537.jpeg",
-};
-
-let post_5 = {
-  author: "Chris Noring",
-  content: "A free book on React, now available as a GitHub repo.",
-  dateCreated: new Date("2021-03-02").toISOString().slice(0, 10),
-  minsToRead: "5",
-  tag: "github, markdown, webdev, readme",
-  title: "I wrote a free book on React.js and just made it available on GitHub",
-  urlAuthor:
-    "https://res.cloudinary.com/practicaldev/image/fetch/s---uM41zbM--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/135219/f7e49646-7ace-4fc2-810e-29e2087e56a0.jpeg",
-  urlPhoto:
-    "https://res.cloudinary.com/practicaldev/image/fetch/s--e1xt_oy4--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/8bhvr7zjf5iugt72ddwo.jpg",
-  social_image:
-    "https://res.cloudinary.com/practicaldev/image/fetch/s--9hY9DQXK-",
-};
-
 $(document).ready(() => {
   console.log("entranod a jqury");
   // baseAllJquery(
